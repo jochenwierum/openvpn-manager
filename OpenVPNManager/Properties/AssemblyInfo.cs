ca3d0b8426bec3df1664cbe918e20bd01ba981da
jochenwierum <jochen@jowisoftware.de>
2009-03-31 15:22:54 +0000

    * Changed version to 0.0.3.1

--- OpenVPNManager/Properties/AssemblyInfo.cs
+++ OpenVPNManager/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.3.0")]
-[assembly: AssemblyFileVersion("0.0.3.0")]
+[assembly: AssemblyVersion("0.0.3.1")]
+[assembly: AssemblyFileVersion("0.0.3.1")]
 
 [assembly: System.CLSCompliant(true)]
 
