1faee207e5de21779ddbd15daa4bd018ce2d7070
Jochen Wierum <jochen@jowisoftware.de>
2012-10-11 10:59:33 +0200

    [DW] bump version to 0.0.3.5

--- OpenVPNManager/Properties/AssemblyInfo.cs
+++ OpenVPNManager/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.3.4")]
-[assembly: AssemblyFileVersion("0.0.3.4")]
+[assembly: AssemblyVersion("0.0.3.5")]
+[assembly: AssemblyFileVersion("0.0.3.5")]
 
 [assembly: System.CLSCompliant(true)]
 
