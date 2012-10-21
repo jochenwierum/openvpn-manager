6c3bae401fd674221b898e2d02ca6b89b250120e
Jochen Wierum <jochen@jowisoftware.de>
2012-10-21 22:12:36 +0200

    [DW] Bumped version

--- OpenVPN/Properties/AssemblyInfo.cs
+++ OpenVPN/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.3.5")]
-[assembly: AssemblyFileVersion("0.0.3.5")]
+[assembly: AssemblyVersion("0.0.3.6")]
+[assembly: AssemblyFileVersion("0.0.3.6")]
 
 [assembly: System.CLSCompliant(true)]
