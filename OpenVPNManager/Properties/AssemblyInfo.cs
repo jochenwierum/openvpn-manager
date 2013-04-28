33e0e686130b4ef46ca431a00231e1f2ad35b5a6
Jochen Wierum <jochen@jowisoftware.de>
2013-04-28 12:50:42 +0200

    [DW] bumped version to 0.0.3.8

--- OpenVPNManager/Properties/AssemblyInfo.cs
+++ OpenVPNManager/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.3.7")]
-[assembly: AssemblyFileVersion("0.0.3.7")]
+[assembly: AssemblyVersion("0.0.3.8")]
+[assembly: AssemblyFileVersion("0.0.3.8")]
 
 [assembly: System.CLSCompliant(true)]
 
